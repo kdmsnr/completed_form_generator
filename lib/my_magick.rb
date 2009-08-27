6c28793797482adcd73a030280315d9abd3986c6
Masanori KADO <kdmsnr@gmail.com>
2009-08-27 12:17:53 +0900

    not fit but fill

--- lib/my_magick.rb
+++ lib/my_magick.rb
@@ -1,13 +1,15 @@
 class Magick::Image
-  def resize_to_fit!(cols, rows)
-    change_geometry(Magick::Geometry.new(cols, rows)) do |ncols, nrows|
-      resize!(ncols, nrows)
-    end
+  def resize_to_fill(ncols, nrows=nil, gravity=CenterGravity)
+    copy.resize_to_fill!(ncols, nrows, gravity)
   end
 
-  def resize_to_fit(cols, rows)
-    change_geometry(Magick::Geometry.new(cols, rows)) do |ncols, nrows|
-      resize(ncols, nrows)
+  def resize_to_fill!(ncols, nrows=nil, gravity=CenterGravity)
+    nrows ||= ncols
+    if ncols != columns || nrows != rows
+      scale = [ncols/columns.to_f, nrows/rows.to_f].max
+      resize!(scale*columns+0.5, scale*rows+0.5)
     end
+    crop!(gravity, ncols, nrows, true) if ncols != columns || nrows != rows
+    self
   end
 end
