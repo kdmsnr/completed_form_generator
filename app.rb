90af0ea0ddc377802350b1a4d8d1dbc8d42c3103
Masanori KADO <kdmsnr@gmail.com>
2009-08-27 22:59:31 +0900

    refactoring: use Card class

--- app.rb
+++ app.rb
@@ -9,25 +9,6 @@ require 'lib/my_magick' # for RMagick 1.x
 include Magick
 
 RIDERS = %w(decade hibiki kabuto den_o kiva kuuga agito ryuki faiz blade)
-CARD_X = 43.0
-CARD_Y = 55.0
-
-Card = Struct.new(:x, :y, :resized_x, :resized_y, :angle)
-cards = {
-  :decade => Card.new(564, 171, CARD_X-1, CARD_Y, -2.5),
-
-  :hibiki => Card.new(402, 432, CARD_X-18, CARD_Y, 13.6),
-  :kabuto => Card.new(434, 440, CARD_X-14, CARD_Y, 13.6),
-  :den_o =>  Card.new(475, 447, CARD_X-14, CARD_Y, 13.4),
-  :kiva =>   Card.new(514, 454, CARD_X-10, CARD_Y, 13.4),
-
-  :kuuga =>  Card.new(567, 460, CARD_X, CARD_Y, 0),
-
-  :agito =>  Card.new(622, 448, CARD_X, CARD_Y, -12),
-  :ryuki =>  Card.new(676, 436, CARD_X, CARD_Y, -12),
-  :faiz =>   Card.new(737, 428, CARD_X, CARD_Y, -10.3),
-  :blade =>  Card.new(790, 419, CARD_X, CARD_Y, -10.3)
-}
 
 class Object
   def blank?
@@ -35,9 +16,44 @@ class Object
   end
 end
 
-def decade
-  ImageList.new('images/decade.jpg')
+class Card
+  include Magick
+
+  X = 43.0
+  Y = 55.0
+
+  def initialize(image)
+    @image = image
+  end
+
+  def generate(user, opt)
+    return @image if user.blank?
+
+    src = ImageList.new(image_src(user)).first
+    src.background_color = "none"
+    src.resize_to_fill!(Card::X, Card::Y).
+      resize!(opt.resized_x, opt.resized_y).
+      rotate!(opt.angle)
+    @image = @image.composite(src, opt.x, opt.y, OverCompositeOp)
+  end
 end
+
+Setting = Struct.new(:x, :y, :resized_x, :resized_y, :angle)
+rider_settings = {
+  :decade => Setting.new(564, 171, Card::X-1, Card::Y, -2.5),
+
+  :hibiki => Setting.new(402, 432, Card::X-18, Card::Y, 13.6),
+  :kabuto => Setting.new(434, 440, Card::X-14, Card::Y, 13.6),
+  :den_o =>  Setting.new(475, 447, Card::X-14, Card::Y, 13.4),
+  :kiva =>   Setting.new(514, 454, Card::X-10, Card::Y, 13.4),
+
+  :kuuga =>  Setting.new(567, 460, Card::X, Card::Y, 0),
+
+  :agito =>  Setting.new(622, 448, Card::X, Card::Y, -12),
+  :ryuki =>  Setting.new(676, 436, Card::X, Card::Y, -12),
+  :faiz =>   Setting.new(737, 428, Card::X, Card::Y, -10.3),
+  :blade =>  Setting.new(790, 419, Card::X, Card::Y, -10.3)
+}
 
 def image_src(user)
   case user
@@ -68,25 +84,6 @@ def image_src(user)
   URI.encode(image)
 end
 
-def generate(result, user, opt)
-  return result if user.blank?
-
-  src = ImageList.new(image_src(user)).first
-  src.background_color = "none"
-  src.resize_to_fill!(CARD_X, CARD_Y).
-    resize!(opt.resized_x, opt.resized_y).
-    rotate!(opt.angle)
-  result = result.composite(src, opt.x, opt.y, OverCompositeOp)
-end
-
-def clean
-  Dir.glob("public/result/*") do |f|
-    if File.mtime(f) < (Time.now - 60*60)
-      File.delete(f)
-    end
-  end
-end
-
 get '/' do
   erb :index
 end
@@ -94,24 +91,11 @@ end
 post '/' do
   params.delete_if {|k, v| !RIDERS.include?(k.to_s) }
 
-  result = decade
-  cards.each do |k, v|
-    result = generate(result, params[k], v)
+  result = ImageList.new('images/decade.jpg')
+  rider_settings.each do |k, v|
+    result = Card.new(result).generate(params[k], v)
   end
 
   content_type :jpg
   return result.to_blob
-
-#  fname = Time.new.to_f
-#  File.open("public/result/#{fname}.jpg", "w") do |f|
-#    f.write(result.to_blob)
-#  end
-#  Thread.new{clean}
-
-#  redirect "/result/#{fname}"
 end
-
-#get '/result/:fname' do
-#  @fname = params[:fname]
-#  erb :result
-#end
